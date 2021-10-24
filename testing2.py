dc7590d5c8b0e2545cb7028a860b840f604c42ad
Ellie <90563871+chancheukyi@users.noreply.github.com>
2021-10-24 17:14:19 +0800

    change and add

--- testing2.py
+++ testing2.py
@@ -1,17 +1,6 @@
 print("hello world")
-
 print("hello world 3")
 
-print("new_branch")
 
-print("new_branch in master")
-
-print("github")
-
-print("hi")
-
-
-print("what")
-
-# testing2
+# branch "testing"
 print("testing2")
