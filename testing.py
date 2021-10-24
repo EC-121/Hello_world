c2b93a5154da9b8f5398004229858e4b511eb877
Ellie <90563871+chancheukyi@users.noreply.github.com>
2021-10-24 17:30:54 +0800

    del

--- testing.py
+++ testing.py
@@ -1,16 +1,1 @@
 print("hello world")
-
-print("hello world 2")
-
-print("hello world 3")
-
-print("new_branch")
-
-print("new_branch in master")
-
-print("github")
-
-print("hi")
-
-
-print("what")
