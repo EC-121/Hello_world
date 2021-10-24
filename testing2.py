a9ee8e7acd5d31090706b68b92a8f0fa26097aec
Ellie <90563871+chancheukyi@users.noreply.github.com>
2021-10-24 16:48:10 +0800

    adding testing2.py

---
+++ testing2.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+print("hello world 3")
+
+print("new_branch")
+
+print("new_branch in master")
+
+print("github")
+
+print("hi")
+
+
+print("what")
+
+# testing2
+print("testing2")
